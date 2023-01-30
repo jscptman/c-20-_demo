515cbb173b11cdbf4b7e6b350afedf04163380ad
jscptman <jscptman@163.com>
2023-01-30 21:10:17 +0800

    demo

---
+++ chapter1/employeeSystem/Employee.cxx
@@ -0,0 +1,32 @@
+module;
+#include <string>
+export module employee;
+namespace Record {
+	const int DefaultStartingSalary { 30'000 };
+	export const int DefaultRaiseAndDemeritAmount { 1'000 };
+	export class Employee {
+		public:
+			Employee(const std::string& firstName,
+					 const std::string& lastName);
+			void promote(int raiseAmount = 1'000); // 加薪方法
+			void demote(int demeritAmount = 1'000); // 降薪方法
+			void hire(); // 雇佣
+			bool isHired() const; // 判断是否雇佣
+			void fire(); // 解雇
+			void display() const; // 展示员工具体信息
+			void setFirstName(const std::string& firstName); // 设置姓氏
+			const std::string& getFirstName() const; // 获取姓氏
+			void setLastName(const std::string& lastName); // 设置名字
+			const std::string& getLastName() const; // 获取名字
+			void setEmployeeNumber(int employeeNumber); // 设置员工编号
+			int getEmployeeNumber() const; // 获取员工编号
+			void setSalary(int newSalary); // 设置薪资
+			int getSalary() const; // 获取薪资
+		private:
+			std::string m_firstName;
+			std::string m_lastName;
+			int m_employeeNumber { -1 };
+			int m_salary { 30'000 };
+			bool m_hired { false };
+	};
+} 
---
+++ chapter1/employeeSystem/EmployeeImpl.cpp
@@ -0,0 +1,73 @@
+module;
+#include <iostream>
+#include "../format"
+
+module employee;
+using namespace std;
+
+namespace Record {
+	Employee::Employee(const string& firstName, const string& lastName)
+		: m_firstName { firstName }, m_lastName { lastName } {
+	}
+
+	void Employee::promote(const int raiseAmount) {
+		setSalary(getSalary() + raiseAmount);
+	}
+
+	void Employee::demote(const int demeritAmount) {
+		setSalary(getSalary() - demeritAmount);
+	}
+
+	void Employee::hire() {
+		m_hired = true;
+	}
+
+	bool Employee::isHired() const {
+		return m_hired;
+	}
+
+	void Employee::fire() {
+		m_hired = false;
+	}
+
+	void Employee::display() const {
+		cout << format("Employee: {}, {}", getLastName(), getFirstName()) << endl;
+		cout << "-------------------------" << endl;
+		cout << (isHired() ? "Current Employee" : "Former Employee") << endl;
+		cout << format("Employee Number: {}", getEmployeeNumber()) << endl;
+		cout << format("Salary: ${}", getSalary()) << endl;
+		cout << endl;
+	}
+
+	void Employee::setFirstName(const string& firstName) {
+		m_firstName = firstName;
+	}
+
+	const string& Employee::getFirstName() const{
+		return m_firstName;
+	}
+
+	void Employee::setLastName(const string& lastName) {
+		m_lastName = lastName;
+	}
+
+	const string& Employee::getLastName() const {
+		return m_lastName;
+	}
+
+	void Employee::setEmployeeNumber(int employeeNumber) {
+		m_employeeNumber = employeeNumber;
+	} 
+
+	int Employee::getEmployeeNumber() const {
+		return m_employeeNumber;
+	}
+
+	void Employee::setSalary(int newSalary) {
+		m_salary = newSalary;
+	}
+
+	int Employee::getSalary() const {
+		return m_salary;
+	}
+}
